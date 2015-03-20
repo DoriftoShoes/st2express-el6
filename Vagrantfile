6614bd26bd497973ddff3172074a1a8c774aad53
manasdk <manasdk@gmail.com>
2015-03-19 19:31:04 -0700

    Clean any unnecesasry repos and add epel-release repo

--- Vagrantfile
+++ Vagrantfile
@@ -23,7 +23,7 @@ Vagrant.configure(VAGRANT_API_VERSION) do |config|
   # config.vm.synced_folder './packs', '/home/maestro/packs', owner: 'maestro', group: 'maestro'
   # config.vm.synced_folder './vagrant', '/home/maestro/vagrant', owner: 'maestro', group: 'maestro'
 
-  #Provision StackStorm
+  # Provision StackStorm
   config.vm.provision :shell, path: 'bootstrap_puppet.sh'
   config.vm.provision :shell, :inline => "puppet apply /vagrant/fullinstall.pp --debug --verbose"
   config.vm.provision :shell, :inline => "st2ctl reload"
--- bootstrap_puppet.sh
+++ bootstrap_puppet.sh
@@ -1,15 +1,40 @@
 service iptables stop
 setenforce permissive
 
+if [ -f /etc/yum.repos.d/cel-6.50-6Server-server.repo ];
+then
+    rm /etc/yum.repos.d/cel-6.50-6Server-server.repo
+    echo "Removed"
+else
+    echo "Nothing to remove."
+fi
+
+if [ -f /etc/yum.repos.d/cisco-epel.repo ];
+then
+    rm /etc/yum.repos.d/cisco-epel.repo
+    echo "Removed"
+else
+    echo "Nothing to remove."
+fi
+
+if [ -f /etc/yum.repos.d/rtp-linux.repo ];
+then
+    rm /etc/yum.repos.d/rtp-linux.repo
+    echo "Removed"
+else
+    echo "Nothing to remove."
+fi
+
 rm -rf /var/cache/yum/yumdb/* && rm -f /var/lib/rpm/__db* && rm -Rf /var/cache/yum/x86_64/6Server/* && rpm --rebuilddb && yum clean dbcache && yum clean all
 
 cp /vagrant/st2_el6.repo /etc/yum.repos.d/
+rpm -ivh http://download.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
 rpm -Uvh http://rbel.frameos.org/rbel6
 rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-el-6.noarch.rpm
 yum clean all
 yum clean dbcache
 yum install -y glibc glibc-headers glibc-devel nscd
-yum install -y nc git rubygems
+yum install -y --skip-broken nc git rubygems
 yum groupinstall -y "Development tools"
 yum install -y puppet
 puppet module install maestrodev-wget
